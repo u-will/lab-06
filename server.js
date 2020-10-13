a1c22aec01e538d6af834cd591d25aa8c9be9811
Ulrich Wilfried Motchoffo Kom <motchoffow@gmail.com>
2020-10-12 22:52:47 -0700

    wether

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT;
 
 app.use(cors());
 
@@ -23,15 +23,39 @@ function Location(city, geoData) {
   this.latitude = geoData[0].lat;
   this.longitude = geoData[0].lon;
 }
-function handleLocation(request , response) {
+function handleLocation(request, response) {
   try {
     const geoData = require('./data/location.json');
     const city = request.query.city;
     const locationData = new Location(city, geoData);
     response.json(locationData);
-  } catch (error){
+  } catch (error) {
     response.status(500).send('Oops you enter the wromg localhost');
   }
+}
+
+
+
+app.get('/weather', heandleWeather);
+
+
+function heandleWeather(req, res) {
+  try {
+    const jsonData = require('./data/weather.json');
+    const temp = [];
+    jsonData.data.forEach(element => {
+      let weather = new Weather(element);
+      temp.push(weather);
+    });
+    res.json(temp);
+  } catch (error) {
+    res.status(500).send('Oops you enter the wrong..');
+  }
+}
+
+function Weather(day) {
+  this.forecast = day.weather.description;
+  this.time = day.temp;
 }
 
 
